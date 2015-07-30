12e6f2668024297983a0238f88d59c4036d017fa
Priyansh <priyansh1990@gmail.com>
2015-07-30 09:51:21 +0530

    Course ra added

--- Microsoft Python/Practice File.py
+++ Microsoft Python/Practice File.py
@@ -2,8 +2,9 @@
 # smallest= None
 #
 # while True:
-# inp = raw_input("Enter the number: ")
-# if inp == "done":break
+#     inp = raw_input("Enter the number: ")
+#     break
+# if inp == "done":
 #
 #     try:
 #         num = float(inp)
@@ -34,7 +35,7 @@
 # print x[14:17]
 #
 # print ('banana\n')*7
-
+#
 # data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
 # pos = data.find('.')
 # print data[pos:pos+3]
@@ -46,8 +47,55 @@
 # b = [4, 5, 6]
 # c = a + b
 # print (c)
-
+#
 # text = "X-DSPAM-Confidence:    0.8475";
 # number = text.find(':')
 # n = text[number + 1:].strip()
 # print float(n)
+
+import SimpleGUICS2Pygame.simpleguics2pygame
+
+# template for "Guess the number" mini-project
+# input will come from buttons and an input field
+# all output for the game will be printed in the console
+
+
+
+# helper function to start and restart the game
+def new_game():
+    # initialize global variables used in your code here
+
+    # remove this when you add your code
+    pass
+
+
+# define event handlers for control panel
+def range100():
+    # button that changes the range to [0,100) and starts a new game
+
+    # remove this when you add your code
+    pass
+
+def range1000():
+    # button that changes the range to [0,1000) and starts a new game
+
+    pass
+
+def input_guess(guess):
+    # main game logic goes here
+
+    # remove this when you add your code
+    pass
+
+
+# create frame
+
+
+# register event handlers for control elements and start frame
+
+
+# call new_game
+new_game()
+
+
+# always remember to check your completed program against the grading rubric
