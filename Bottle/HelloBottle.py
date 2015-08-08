558a767b9c3d30ff78b5cf9403a3308b442d2539
Priyansh <priyansh1990@gmail.com>
2015-08-08 21:25:54 +0530

    Course ra added

---
+++ Bottle/HelloBottle.py
@@ -0,0 +1,30 @@
+__author__ = 'Pri'
+
+# from bottle import route,template,run
+#
+# @route('/hello')
+# def hello():
+# return "Hello World"
+#
+# run(host='localhost', port=7070, debug=True)
+#
+
+import pymongo
+
+from pymongo import MongoClient
+
+
+# connect to database
+connection = MongoClient('localhost', 27017)
+
+db = connection.test
+
+# handle to names collection
+names = db.names
+
+item = names.find_one()
+
+print item['name']
+
+a=["Apple","Banana"];
+print (a)
--- Coursera/RPS.py
+++ Coursera/RPS.py
@@ -1,17 +1,5 @@
 __author__ = 'Pri'
 
-# Rock-paper-scissors-lizard-Spock template
-
-# The key idea of this program is to equate the strings
-# "rock", "paper", "scissors", "lizard", "Spock" to numbers
-# as follows:
-#
-# 0 - rock
-# 1 - Spock
-# 2 - paper
-# 3 - lizard
-# 4 - scissors
-
 # helper functions
 import random
 
