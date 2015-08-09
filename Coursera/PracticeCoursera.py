8fb53ac1fe0d754f8881e5fc5253d09436f697fa
Priyansh <priyansh1990@gmail.com>
2015-08-09 18:36:11 +0530

    Dictionaries

--- Coursera/PracticeCoursera.py
+++ Coursera/PracticeCoursera.py
@@ -1,1 +1,26 @@
 __author__ = 'Pri'
+a = ["apple", "banana", "pear"]
+print(a)
+
+h = [1, 2, 3]
+# print(h)
+print type(h)
+
+c = [1, a, 2, 3]
+# print(c[0:])
+
+# print(c[:])
+
+things = ['apples', 'orange', 'pear', 'grape', 'kiwi']
+# print(things[1:3])
+
+if 'pear' in things:
+    print("There is")
+
+if not 'mango' in things:
+    print("No")
+
+a = {'name': 'Priyansh', 'Fruits': things}
+
+# del a['name']
+print(a['Fruits'][0:2])
