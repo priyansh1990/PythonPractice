d768b8e79be7d86feffa2558b6dfcbac44e435b7
Priyansh <priyansh1990@gmail.com>
2015-08-09 19:20:07 +0530

    Dictionaries with loops

--- Coursera/PracticeCoursera.py
+++ Coursera/PracticeCoursera.py
@@ -1,10 +1,10 @@
 __author__ = 'Pri'
 a = ["apple", "banana", "pear"]
-print(a)
+# print(a)
 
 h = [1, 2, 3]
 # print(h)
-print type(h)
+# print type(h)
 
 c = [1, a, 2, 3]
 # print(c[0:])
@@ -14,30 +14,40 @@ c = [1, a, 2, 3]
 things = ['apples', 'orange', 'pear', 'grape', 'kiwi']
 # print(things[1:3])
 
-if 'pear' in things:
-    print("There is")
+# if 'pear' in things:
+# print("There is")
 
-if not 'mango' in things:
-    print("No")
+# if not 'mango' in things:
+# print("No")
 
 a = {'name': 'Priyansh', 'Fruits': things}
 
 # del a['name']
-print(a['Fruits'][0:2])
+# print(a['Fruits'][0:2])
 
 a['Fruits'].append('Mango')
 
-print(a)
+# print(a)
 
 # myArray = [[1, 2], [3, 4]]
 #
 # print(myArray)
 
-new_fruit = []
+# new_fruit = []
+#
+# for item in things:
+# print(item)
+# new_fruit.append(item)
+#
+# # print(new_fruit)
 
-for item in things:
-    print(item)
-    new_fruit.append(item)
+# person = {'name': 'priyansh', 'surname': 'agrwal', 'color': 'blue', 'hair': 'black'}
+#
+# for key in person:
+#     print key + ' Value is ' + (person[key])
 
-print(new_fruit)
+# i = 0
+# while i < len(things):
+#     print(things[i])
+#     i += 1
 
